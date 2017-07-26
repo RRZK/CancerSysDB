1bc362f2553c01aa2383d86b45bc62e33a54cd18
krempelr <r.krempel@uni-koeln.de>
2017-07-26 17:33:00 +0200

    docker Startup bug fixed

--- docker/openImage/run.sh
+++ docker/openImage/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-while ! $(mysql -h CancersysMariaDB -ucsysUser -pcsysPassword  -e "select 1" cancersys;)
+CHECKCOMMANDMYSQL="mysql -h CancersysMariaDB -ucsysUser -pcsysPassword  -e \"select 1\" cancersys;"
+while ["$CHECKCOMMANDMYSQL" -ne 0]
 do
     echo "Waiting for Mysql to Come up!"
-    sleep 60
+    sleep 20
 done
 /etc/init.d/tomcat7 start
 exec tail -f /var/log/tomcat7/catalina.out
